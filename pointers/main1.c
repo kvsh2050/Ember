81b4b2c2da6c1a774a8159924e29d9b6e1a3e044
kavyash <kavyashree.r@pinacagroup.com>
2025-12-09 18:33:32 +0530

    Pointers update dec 9 2025

--- pointers/main1.c
+++ pointers/main1.c
@@ -4,9 +4,10 @@ int main(){
     // Pointer declaration 
     char c = 'A';
     char *ptr = &c ;
+    char **pptr = &ptr;
 
     printf("c = %c\n&c = %p\n", c , &c);
-    printf("ptr = %p\ndef_ptr = %c\n&ptr = %p\n", ptr , *ptr, &ptr);
+    printf("ptr = %p\ndef_ptr = %c\n&ptr = %p\npptr = %p\n", ptr , *ptr, &ptr, pptr);
 
     return 0;
 }
